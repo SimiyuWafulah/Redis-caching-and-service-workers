a0bb1ad3e625c3677be9b1ffb4b2abe2760a822d
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-07-30 23:28:34 +0300

    sets up redis connection

--- backend-ts/index.ts
+++ backend-ts/index.ts
@@ -1,7 +1,17 @@
 import express from 'express'
+import redis from 'redis';
 
 const app = express();
 const PORT = 3000;
+const client = redis.createClient();
+
+client.on('error', (err) => {
+    console.log('Redis error:', err);
+});
+
+client.on('connect', () => {
+    console.log('Connected to Redis');
+});
 
 app.listen(PORT, () => {
     console.log('Server is Running')
